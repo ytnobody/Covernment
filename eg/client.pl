7bd89bcd1092c4050eebdb11fd17a6701144c2ac
ytnobody <ytnobody@gmail.com>
2016-07-09 10:36:20 +0900

    remove unused code

--- eg/client.pl
+++ eg/client.pl
@@ -6,9 +6,6 @@ use lib (
     File::Spec->catdir(dirname(__FILE__), qw/.. lib/),
 );
 use Covernment::Client;
-use Data::Dumper;
-
-$Covernment::Client::CLEANUP = 1;
 
 Covernment::Client->upload_project(
     File::Spec->catfile(dirname(__FILE__), qw/../)
